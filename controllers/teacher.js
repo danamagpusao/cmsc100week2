0c52d071265695302971947a46e719fbe2d9e354
danamagpusao <danamagpusao818@gmail.com>
2015-08-28 11:46:07 +0800

    creates a nodejs

---
+++ controllers/teacher.js
@@ -0,0 +1,15 @@
+exports.find = function (req,res){
+    res.send("Hello World");
+;}
+
+exports.create = function(req,res){
+    res.send("create a teacher");
+;}
+
+exports.update= function(req,res){
+    res.send("update a teacher");
+;}
+
+exports.delete = function(req,res){
+    res.send("delete a teacher");
+;}
---
+++ index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var app = express();
+
+var server = app.listen(5000,function (){
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log('Example app listening at http://%s:%s',host,port);
+    
+});
+//if client requests 
+//app.get ('/', function (req,res) {
+ //       res.send ('hello World');
+//});
+
+
+/*app.route('/Students')
+       .get(function (req,res){
+            res.send('Get a student');
+        })
+	   .post(function (req,res){
+	   		res.send('Post a student');
+	    })
+		.put(function (req,res){
+			res.send("Update a student");
+		})
+;*/
+
+app.use(require('body-parser') ());
+app.use(require('method-override')());
+app.use(require(__dirname + '/config/router') (express.Router())); //dirname where the file being executed is stored
+ 
