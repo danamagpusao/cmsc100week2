b704bc4406c69b128e1aa351d21d61f9a236e5dc
danamagpusao <danamagpusao818@gmail.com>
2015-09-04 12:37:29 +0800

    added mysql

--- config/router.js
+++ config/router.js
@@ -2,8 +2,9 @@ var student = require('./../controllers/student');
 var teacher = require('./../controllers/teacher');
 
 module.exports = function(router){
-    router.route('/student')
-        .get(student.find);
+    router.route('/students')
+        .get(student.find)
+        .post(student.insert);
     router.route('/teacher')
         .get(teacher.find);
     router.route('/teacher')
@@ -12,6 +13,11 @@ module.exports = function(router){
         .put(teacher.update);
     router.route('/teacher')
         .delete(teacher.delete);
+        
+   router.route('/students/:id') //expects a parameter
+        .get(student.findOne)
+        .put(student.update)
+        .delete(student.remove);
         return router;
         
 }
--- controllers/student.js
+++ controllers/student.js
@@ -1,3 +1,62 @@
-exports.find = function (req,res){
-    res.send("Hello World");
-;}
+var db = require(__dirname+'/../lib/mysql');
+
+    exports.find = function(req,res,next){
+        db.query('SELECT * FROM student',
+          function(err,rows){
+            if(err) return next(err);
+            res.send(rows);
+          });
+    };
+    
+    exports.findOne = function(req,res,next){
+        db.query('SELECT * FROM student WHERE studNo=?',
+            [req.params.id],
+            function(err,rows){
+                if(rows.length===0) res.status(404).send("STUDENT NOT FOUND!");
+                else res.send(rows[0]);
+                if(err) return next(err);
+                res.send(rows);
+               
+            });
+    
+    
+    };
+    
+    exports.insert = function(req,res,next){
+        db.query('INSERT INTO student VALUES(?,?,?)', [req.body.studNo,req.body.name,req.body.bdate],
+        function(err,rows){
+                if(rows.length===0) res.status(404).send("STUDENT NOT FOUND!");
+                else res.send(rows[0]);
+                if(err) return next(err);
+                res.send(rows);
+               
+            }
+        );
+    
+    };
+    
+      exports.update = function(req,res,next){
+        db.query('UPDATE student SET ? WHERE studNo=?', [req.body,req.params.id],
+        function(err,rows){
+                if(rows.length===0) res.status(404).send("STUDENT NOT FOUND!");
+                else res.send(rows[0]);
+                if(err) return next(err);
+                res.send(rows);
+               
+            }
+        );
+    
+    };
+
+    exports.remove = function(req,res,next){
+        db.query('DELETE FROM student WHERE studNo=?', [req.params.id],
+        function(err,rows){
+                if(rows.length===0) res.status(404).send("STUDENT NOT FOUND!");
+                else res.send(rows[0]);
+                if(err) return next(err);
+                res.send(rows);
+               
+            }
+        );
+    
+    };
